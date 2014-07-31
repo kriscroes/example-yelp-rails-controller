0fefa59be02eefdbb203f23d6c3a3d81cbdaceef
kriscroes <k_croes@hotmail.com>
2014-07-30 23:51:16 -0400

    example rails foods controller

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = "jHrX6GUAL8F4P5aGOIpf6A"
+  config.consumer_secret = "5RLAVd4HU5RBMeQ-FSMxDPL16J4"
+  config.token = "cFRqWMVbOZv7tLOpQnjh5zvznm0MZ5HH"
+  config.token_secret = "8KQvty31tGl_CNwuo8Zw3Dd8Hw4"
+end
